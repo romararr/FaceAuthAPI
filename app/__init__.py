e30a0e3800092368ae301d1720bfa824c99255ce
Rama Romara <romara17.rr@gmail.com>
2020-03-17 17:11:15 +0700

    add missing init model in main init.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,9 @@ from flask_migrate import Migrate
 from flask_seeder import FlaskSeeder
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # config database connection
-app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -15,5 +15,5 @@ migrate = Migrate(app, db)
 seeder = FlaskSeeder()
 seeder.init_app(app, db)
 
-from app.models import user
+from app.models import order, orderStuff, user
 from app import routes
